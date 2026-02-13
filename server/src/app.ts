011ff29fb843a8276c5d994f13fdea88fe76b9a4
Omji Kushwaha <omjimaurya09@gmail.com>
2026-02-13 15:59:21 +0530

    add server build command

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,10 @@
 import express from "express";
+import type { Express } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { specRouter } from "./routes/spec.routes";
 
-export const app = express();
+export const app: Express = express();
 
 app.use(cors());
 app.use(bodyParser.json());
